ed6b23203f5e773514ac52c02af38b79469cf184
Logan Woolf <loganwoolf@gmail.com>
2023-12-02 14:36:56 -0700

    Set up main layout

---
+++ src/lib/nav.ts
@@ -0,0 +1,16 @@
+const navItems: { name: string; path: string }[] = [
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name: 'About',
+    path: '/about'
+  },
+  {
+    name: 'Contact',
+    path: '/contact'
+  }
+]
+
+export { navItems }
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '$lib/app.css';
+	import { navItems } from '$lib/nav';
+</script>
+
+<header>
+	<nav>
+		{#each navItems as link}
+			<a href={link.path}>{link.name}</a>
+		{/each}
+	</nav>
+</header>
+
+<slot />
+
+<footer>
+	<p>Made in 🍁 by Logan Woolf</p>
+</footer>
